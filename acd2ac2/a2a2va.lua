9e2a1c95de0aef48c0db42b730a39fc2d142e346
BassDarius <73274843+BassDarius@users.noreply.github.com>
2022-11-06 17:49:22 +0200

    Update and rename 2va2a.lua to a2a2va.lua

--- acd2ac2/a2a2va.lua
+++ acd2ac2/a2a2va.lua
@@ -549,8 +549,8 @@ do -- Player Metatable
                         if Chams_Enabled then
                             Chams.FillColor = Chams_Settings.Mode == "Visible" and Is_Visible and Color3.new(1, 0, 0) or Chams_Settings.Color
                             Box.Color = Chams_Settings.Mode == "Visible" and Is_Visible and Color3.new(1, 0, 0) or Chams_Settings.Color
-                            Name.Color = Chams_Settings.Mode == "Visible" and Is_Visible and Color3.new(1, 0, 0) or Chams_Settings.Color
-                            Tool.Color = Chams_Settings.Mode == "Visible" and Is_Visible and Color3.new(1, 0, 0) or Chams_Settings.Color
+                            Name.OutlineColor = Chams_Settings.Mode == "Visible" and Is_Visible and Color3.new(1, 0, 0) or Chams_Settings.Color
+                            Tool.OutlineColor = Chams_Settings.Mode == "Visible" and Is_Visible and Color3.new(1, 0, 0) or Chams_Settings.Color
                
                             Chams.OutlineColor = Chams_Settings.OutlineColor
                             Chams.FillTransparency = Chams_Settings.Transparency
