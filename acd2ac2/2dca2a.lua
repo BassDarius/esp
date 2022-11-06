ecfa8e1ff997108bf04a1b5e5bd867fa4535ef05
BassDarius <73274843+BassDarius@users.noreply.github.com>
2022-11-06 17:33:11 +0200

    Update and rename asdca.lua to 2dca2a.lua

--- acd2ac2/2dca2a.lua
+++ acd2ac2/2dca2a.lua
@@ -551,6 +551,8 @@ do -- Player Metatable
                             Box.Color = Chams_Settings.Mode == "Visible" and Is_Visible and Color3.new(1, 0, 0) or Chams_Settings.Color
                             Name.Color = Chams_Settings.Mode == "Visible" and Is_Visible and Color3.new(1, 0, 0) or Chams_Settings.Color
                             Tool.Color = Chams_Settings.Mode == "Visible" and Is_Visible and Color3.new(1, 0, 0) or Chams_Settings.Color
+                            Name.OutlineColor = Chams_Settings.Mode == "Visible" and Is_Visible and Color3.new(1, 0, 0) or Chams_Settings.Color
+                            Tool.OutlineColor = Chams_Settings.Mode == "Visible" and Is_Visible and Color3.new(1, 0, 0) or Chams_Settings.Color
                             Chams.OutlineColor = Chams_Settings.OutlineColor
                             Chams.FillTransparency = Chams_Settings.Transparency
                             Chams.OutlineTransparency = Chams_Settings.OutlineTransparency
